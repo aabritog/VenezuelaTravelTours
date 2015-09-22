255019203ff38a3a1e3e340b9f2dac7ab9be2209
aabritog <ing.aabg@gmail.com>
2015-09-22 19:23:34 -0430

    Directorio
    Ajustes en el directorio raiz

---
+++ en/VenezuelaTravelTours/menu.php
@@ -0,0 +1,31 @@
+<!DOCTYPE HTML>
+
+<html>		
+						<nav id="nav">
+							<ul>
+								<li><a href="index.php" id="logo"><img src="images/Logo-Final.png" width="170px" height="150px" alt="Logo" /></a></li>
+								<li><a href="index.php">Home</a></li>
+								<li><a href="quienes-somos.php">About us</a></li>
+								<li>
+									<a>Destinations</a>
+									<ul>
+										<li><a href="parque-canaima.php">Canaima</a></li>
+										<li><a href="orinoco-delta.php">Orinoco Delta</a></li>
+										<li><a href="isla-de-margarita.php">Margarita</a></li>
+										<li><a href="estado-merida.php">Merida</a></li>
+										<li><a href="los-llanos-venezuela.php">The Llanos</a></li>																				
+										<li><a href="los-roques.php">The Roques</a></li>
+										<li><a href="salto-angel.php">Angel Falls</a></li>										
+									</ul>
+								</li>
+								<li><a href="#contacto" class="scrolly">Contact</a></li>
+							</ul>
+							<div style="position:absolute; top:0; right:0; padding: 15px;">
+								<br>
+								<!--<a href="<?php echo str_replace('/en/','/',$_SERVER['SCRIPT_NAME']) ?>"><img src="images/flags/shiny/32/Spain.png" alt="Español" title="Español"></a>
+								<a href="<?php echo str_replace('/es/','/en/',$_SERVER['SCRIPT_NAME']) ?>"><img src="images/flags/shiny/32/United-States.png" alt="English" title="English"></a>-->
+								<a href="<?php echo str_replace('VenezuelaTravelTours/en/VenezuelaTravelTours/','VenezuelaTravelTours/',$_SERVER['SCRIPT_NAME']) ?>"><img src="images/flags/shiny/32/Spain.png" alt="Español" title="Español"></a>								
+								<a href="<?php echo 'en'.$_SERVER['SCRIPT_NAME'] ?>"><img src="images/flags/shiny/32/United-States.png" alt="English" title="English"></a>								
+							</div>
+						</nav>								
+
